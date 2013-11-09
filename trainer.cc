2f6b992452707d4dd76a5fcf7ef8edaf20c4feca
Daniel Sabinasz <danielsabinasz@danielsmacbook.speedport_w_723v_1_32_000>
2013-11-09 15:22:01 +0100

    Relative word frequencies

--- trainer.cc
+++ trainer.cc
@@ -38,6 +38,10 @@ int main(int argc, char **argv) {
 	typedef map<string, word_counts_map> class_word_counts_map;
 	class_word_counts_map word_counts;
 
+	// Track total word count per class
+	typedef map<string, unsigned int> class_total_word_counts_map;
+	class_total_word_counts_map total_word_counts;
+
 	// Iterate file line by line (each line represents a document)
 	string document;
 	while (getline(f, document)) {
@@ -72,7 +76,36 @@ int main(int argc, char **argv) {
 				word_counts[document_class][word_id] = 0;
 			}
 			word_counts[document_class][word_id]++;
+
+			// Increase class-wide total word count
+			if (total_word_counts.count(document_class) == 0) {
+				total_word_counts[document_class] = 0;
+			}
+			total_word_counts[document_class]++;
 		}
+	}
+
+	// Iterate all classes
+	for (class_word_counts_map::iterator it = word_counts.begin(); it != word_counts.end(); it++) {
+		string class_name = it->first;
+		int total_word_count = total_word_counts[class_name];
+
+		cout << class_name << ":" << endl;
+
+		// Iterate all words for that class
+		for (word_counts_map::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+			int word_id = it2->first;
+			int word_count = it2->second;
+
+			// Determine relative frequency of that word in the class
+			// (stored as negative logarithm for numeric stability;
+			// this is the same as -log(word_count/total_word_count)
+			double rel_freq = log(total_word_count) - log(word_count);
+
+			cout << dict->word(word_id) << " " << rel_freq << endl;
+		}
+
+		cout << endl;
 	}
 
 }
